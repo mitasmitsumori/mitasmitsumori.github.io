f63385c01727e8a93174d2043e276fb1cb33a396
mitasmitsumori <bully354.8@gmail.com>
2025-06-17 22:13:25 +0900

    settings.js を更新

--- settings.js
+++ settings.js
@@ -1,24 +1,36 @@
-// 単価などをまとめて管理
-const MiTAS_SETTINGS = {
-  demolitionUnitPrice: {
-    木造: 33000,
-    RC造: 50000,
-    S造: 33000,
-    軽量鉄骨造: 33000
+// settings.js
+
+const PRICES = {
+  demolitionPerTsubo: {
+    "木造": 33000,
+    "RC造": 50000,
+    "S造": 33000,
+    "軽量鉄骨造": 33000,
   },
-  scaffoldUnitPrice: 600,
-  machineCost: 14000,
-  asbestosSurveyCost: 90000,
-  wasteUnitPrices: {
+  scaffoldPerM2: 600,
+  asbestosSurvey: 90000,
+  machineTransport: {
+    short: 14000,  // 10km以内
+    medium: 18000, // 10km以上
+    long: 35000,   // 50〜60km
+  },
+  wasteDisposal: {
     wood: 16,
     board: 35,
     plastic: 50,
     rubble: 2,
     glass: 10,
-    ceramic: 20
+    ceramic: 20,
   },
-  metalBuyback: {
+  resaleMaterials: {
     iron: 25,
-    aluminum: 100
+    aluminum: 100,
+  },
+  transportation: {
+    wood: 2000,
+    concrete: 1000,
+    plastic: 2000,
+    glassCeramic: 2000,
+    board: 2000,
   }
 };
