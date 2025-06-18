0f0d70552583ae20277862237be2646822977d5a
mitasmitsumori <bully354.8@gmail.com>
2025-06-18 23:08:34 +0900

    settings.js を更新

--- settings.js
+++ settings.js
@@ -1,31 +1,36 @@
+// 単価設定（変更可能）
 const SETTINGS = {
-  demolitionUnitPrice: {
+  structureUnitPrices: {
     "木造": 33000,
     "RC造": 50000,
     "S造": 33000,
     "軽量鉄骨造": 33000
   },
-  scaffoldUnitPrice: 600,
-  surveyCost: 90000,
-  machineTransportCost: {
-    short: 14000,
-    long: 18000,
-    distant: 35000
+  scaffoldUnitPrice: 600, // 円/㎡
+  asbestosSurveyFee: 90000, // 固定費
+
+  machineTransportFee: {
+    "10km以内": 14000,
+    "10km以上": 18000,
+    "50〜60km": 35000
   },
-  wasteUnitPrice: {
-    "木くず": 16,
+
+  disposalUnitPrices: {
+    "木くず": 16,       // 円/kg
     "ボード類": 35,
     "廃プラ類": 50,
     "ガラ": 2,
     "ガラス": 10,
     "陶器": 20
   },
-  valuableItems: {
-    "鉄": 25,
+
+  recyclePurchasePrices: {
+    "鉄": 25,    // 円/kg
     "アルミ": 100
   },
-  transportCostPerCubic: {
-    "木くず": 2000,
+
+  transportVolumePrices: {
+    "木くず": 2000,    // 円/㎥
     "コンクリート": 1000,
     "廃プラ": 2000,
     "ガラス・陶器": 2000,
