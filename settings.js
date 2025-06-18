df5ccccda16b05c0207cd96f22364a19c6d2c206
mitasmitsumori <bully354.8@gmail.com>
2025-06-18 12:13:25 +0900

    settings.js を更新

--- settings.js
+++ settings.js
@@ -1,27 +1,17 @@
+// settings.js
+
 const SETTINGS = {
-  demolitionUnitPrices: {
+  unitPrices: {
     "木造": 33000,
     "RC造": 50000,
     "S造": 33000,
-    "軽量鉄骨造": 33000
-  },
-  scaffoldUnitPrice: 600,
-  asbestosSurveyFee: 90000,
-  machineTransportFee: {
-    within10km: 14000,
-    over10km: 18000,
-    over50km: 35000
-  },
-  sanpaiPrices: {
-    wood: 16,
-    board: 35,
-    plastic: 50,
-    rubble: 2,
-    glass: 10,
-    ceramic: 20
-  },
-  recyclablePrices: {
-    iron: 25,
-    aluminum: 100
+    "軽量鉄骨造": 33000,
+    "足場": 600,
+    "木くず": 16,
+    "ボード類": 35,
+    "廃プラ類": 50,
+    "ガラ": 2,
+    "ガラス": 10,
+    "陶器": 20
   }
 };
