aeff00edce4201c931203e748891400dfdbec212
mitasmitsumori <bully354.8@gmail.com>
2025-06-28 08:01:49 +0900

    settings.js を更新

--- settings.js
+++ settings.js
@@ -1,17 +1,23 @@
 const SETTINGS = {
-  demolitionUnitPrices: {
+  unitPrices: {
     "木造": 33000,
     "RC造": 50000,
     "S造": 33000,
-    "軽量鉄骨造": 33000
-  },
-  scaffoldUnitPrice: 600,
-  wasteDisposalUnitPrices: {
+    "軽量鉄骨造": 33000,
+    "足場": 600,
     "木くず": 16,
     "ボード類": 35,
     "廃プラ類": 50,
     "ガラ": 2,
     "ガラス": 10,
     "陶器": 20
+  },
+  kgPerSquareMeter: {
+    woodWaste: 2.15,
+    boardWaste: 1.42,
+    plasticWaste: 0.48,
+    rubbleWaste: 0.19,
+    glassWaste: 0.11,
+    ceramicWaste: 0.06
   }
 };
